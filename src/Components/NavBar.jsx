695376b5c60cb8ac208459fb5884e00fa6ced56d
Ed <lookatemail@gmail.com>
2020-09-01 19:53:16 +0100

    Add main colors to the project

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,10 +4,24 @@ export default function NavBar({ props }) {
   return (
     <div style={styles.container}>
       <div>NavBar</div>
+      <div>NavBar</div>
+      <div>NavBar</div>
+      <div>NavBar</div>
+      <div>NavBar</div>
     </div>
   );
 }
 
 const styles = {
-  container: {},
+  container: {
+    position: 'relative',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(5, 1fr)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100vw',
+    minHeight: 50,
+    textAlign: 'center',
+    backgroundColor: 'blue',
+  },
 };
