36721422a2112dc20b17f4a829e2bead0ab37f50
Ed <lookatemail@gmail.com>
2020-09-01 19:24:01 +0100

    Create NavBar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavBar from './Components/NavBar';
 
 export default function App({ props }) {
   return (
     <div style={styles.container}>
+      <NavBar />
       <div>Hello World</div>
     </div>
   );
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NavBar({ props }) {
+  return (
+    <div style={styles.container}>
+      <div>NavBar</div>
+    </div>
+  );
+}
+
+const styles = {
+  container: {},
+};
